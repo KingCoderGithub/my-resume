04a5b69df22edc67ec6b51cd9cd4b1c84c7588b7
KingCoderGithub <84721340+KingCoderGithub@users.noreply.github.com>
2025-06-16 14:19:58 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
 const nextConfig: NextConfig = {
   output: 'export', // Required for static export
   // basePath and assetPrefix are needed if you're deploying to a subpath on GitHub Pages
-  // e.g., https://YOUR_USERNAME.github.io/YOUR_REPOSITORY_NAME/
+  // e.g., https://KingCoderGithub.github.io/my-resume/
   // If deploying to a custom domain or a root path, these can be removed or set to ''.
   basePath: isGithubActions ? `/${GITHUB_REPOSITORY_NAME}` : '',
   assetPrefix: isGithubActions ? `/${GITHUB_REPOSITORY_NAME}/` : '',
